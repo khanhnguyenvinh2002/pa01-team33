c37d3854a892444c829e0f414b75fdeaf789a80c
khanhnguyenvinh2002 <giakhanhvy@gmail.com>
2022-03-23 03:58:20 -0400

    commit

--- course_search.py
+++ course_search.py
@@ -2,13 +2,12 @@
 course_search is a Python script using a terminal based menu to help
 students search for courses they might want to take at Brandeis
 '''
-
 from schedule import Schedule
-import sys
 
-schedule = Schedule()
-schedule.load_courses()
-schedule = schedule.enrolled(range(5,1000)) # eliminate courses with no students
+
+SCHEDULE = Schedule()
+SCHEDULE.load_courses()
+SCHEDULE = SCHEDULE.enrolled(range(5,1000)) # eliminate courses with no students
 
 TOP_LEVEL_MENU = '''
 quit
@@ -22,48 +21,70 @@ description (filter by phrase in description)
 timeofday (filter by day and time, e.g. meets at 11 on Wed)
 '''
 
-terms = {c['term'] for c in schedule.courses}
+terms = {c['term'] for c in SCHEDULE.courses}
 
 def topmenu():
     '''
     topmenu is the top level loop of the course search app
     '''
-    global schedule
-    while True:         
+    global SCHEDULE
+    while True:
         command = input(">> (h for help) ")
         if command=='quit':
             return
-        elif command in ['h','help']:
+        if command in ['h','help']:
             print(TOP_LEVEL_MENU)
             print('-'*40+'\n\n')
             continue
         elif command in ['r','reset']:
-            schedule.load_courses()
-            schedule = schedule.enrolled(range(5,1000))
+            SCHEDULE.load_courses()
+            SCHEDULE = SCHEDULE.enrolled(range(5,1000))
             continue
         elif command in ['t', 'term']:
             term = input("enter a term:"+str(terms)+":")
-            schedule = schedule.term([term]).sort('subject')
+            SCHEDULE = SCHEDULE.term([term]).sort('subject')
         elif command in ['s','subject']:
             subject = input("enter a subject:")
-            schedule = schedule.subject([subject])
+            SCHEDULE = SCHEDULE.subject([subject])
+        elif command in ['c', 'courses']:
+            course = input("enter a coursenum:")
+            SCHEDULE = SCHEDULE.course_num([course])
+        elif command in ['i', 'instructor']:
+            instructor = input('enter instructor\'s lastname to see courses they teach')
+            SCHEDULE = SCHEDULE.lastname([instructor])
+        elif command in ['ti', 'title']:
+            title = input('enter a title')
+            SCHEDULE = SCHEDULE.title([title])
+        elif command in ['d', 'description']:
+            description = input('enter a description')
+            SCHEDULE = SCHEDULE.description([description])
+        elif command in ['n', 'name']:
+            course_name = input("enter a course name:")
+            SCHEDULE = SCHEDULE.name([course_name])
+        elif command in ['e', 'enrolled']:
+            course_enrolled = input("enter a number of students:")
+            SCHEDULE = SCHEDULE.enrolled([course_enrolled])
+        elif command in ['l', 'limit']:
+            course_limit = input("enter a number of students:")
+            SCHEDULE = SCHEDULE.limit([course_limit])
         else:
             print('command',command,'is not supported')
             continue
-
-        print("courses has",len(schedule.courses),'elements',end="\n\n")
+        print("courses has",len(SCHEDULE.courses),'elements',end="\n\n")
         print('here are the first 10')
-        for course in schedule.courses[:10]:
+        for course in SCHEDULE.courses[:10]:
             print_course(course)
         print('\n'*3)
 
 def print_course(course):
-    '''
-    print_course prints a brief description of the course 
-    '''
+    '''print_course prints a brief description of the course '''
     print(course['subject'],course['coursenum'],course['section'],
           course['name'],course['term'],course['instructor'])
 
+def print_course_subject(course):
+    print(course['subject'])
+
+
+
 if __name__ == '__main__':
     topmenu()
-
